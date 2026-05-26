c7d3a7a18b82da6b14cfcd737b3ba78b1280c3ef
Walnut <39544927+Walnut356@users.noreply.github.com>
2026-05-26 03:01:19 -0500

    temp3

--- src/etc/lldb_test.py
+++ src/etc/lldb_test.py
@@ -1,14 +1,20 @@
-from __future__ import annotations
+# IMPORTANT: we cannot use from __future__ import annotations because we rely on the runtime class
+# information from type hints. `from __future import annotations` converts type hints to strings,
+# and it's a bit more trouble to pull the class information that way.
+# from __future__ import annotations
 
+from typing import get_origin
+from dataclasses import field
 import sys
 import enum
 import os
 import json
+import pprint
 
 from collections import UserDict
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, is_dataclass, fields
 from struct import unpack
-from typing import TypeAlias, Optional
+from typing import TypeAlias, Optional, Any, Self
 
 import lldb_lookup
 import lldb
@@ -17,9 +23,13 @@ from lldb import SBDebugger, SBValue, SBType, SBData, eBasicTypeInvalid
 char: TypeAlias = str
 Primitive: TypeAlias = int | float | bool | char
 
-FRAME = lldb.frame
+# see: default json decoder docs https://docs.python.org/3/library/json.html#json.JSONDecoder
+# The types we're dealing with can only be: int, str, float, list, dict, bool, and None
+JsonType: TypeAlias = int | str | float | list["JsonType"] | bool | None | dict[str, "JsonType"]
+# JsonDict: TypeAlias = dict[str, JsonType]
 
-print(os.environ)
+FRAME = lldb.frame
+BLESS = os.environ["LLDB_BATCHMODE_BLESS_TEST_DATA"] == "1"
 
 
 class Target(enum.StrEnum):
@@ -33,9 +43,9 @@ class Target(enum.StrEnum):
     A global var `TARGET` is set to the current variant upon `lldb_test.py`'s instantiation using an
     env var passed from `compiletest` and is not expected to change afterwards."""
 
-    NonWindowsGnu = "NonWindowsGnu"
-    WindowsGnu = "WindowsGnu"
-    WindowsMsvc = "WindowsMsvc"
+    NonWindowsGnu = "non_windows_gnu"
+    WindowsGnu = "windows_gnu"
+    WindowsMsvc = "windows_msvc"
 
 
 def get_target() -> Target:
@@ -47,69 +57,65 @@ def get_target() -> Target:
     if t.endswith("windows-msvc"):
         return Target.WindowsGnu
 
-    return Target.NonWindowsGnu
+    if t.endswith("-gnu"):
+        return Target.NonWindowsGnu
+
+    raise Exception(f"Unable to classify target '{t}'")
 
 
 TARGET: Target = get_target()
-"""The target this test was compiled for. This variable should never change at runtime."""
+"""The target this test was compiled for. This variable should never be changed after
+initialization."""
 
 
-class RawInputData(UserDict[Target | str, list[dict[str, dict]]]):
-    """The input data is structured as follows:
+def from_dict(ty: type[Any], data: JsonType):
+    """Translates a dictionary into an instance of the given dataclass type (with possibly nested
+    dataclasses).
 
-    * The outer dict maps each target to a list
-    * The list is indexed by which breakpoint we're at (supplied by lldb_batchmode)
-    * Each list element is a dict mapping variable names to variable data
+    Relies on accurate type hints for the dataclass's fields, and the standard `dataclass.__init__`
+    definition."""
+    if get_origin(ty) is Optional:
+        ty = ty.__args__[0]
 
-    A global var `INPUT_DATA` is read from the file name supplied by `compiletest` in an env var. If
-    the file does not exist, `INPUT_DATA` contains a default mapping of the targets to empty lists.
+    # recurse into lists
+    if isinstance(data, list):
+        # pulls the generic type from the list (e.g. `list[int]` -> `int`)
+        (inner,) = ty.__args__
+        print(inner)
+        return [from_dict(inner, i) for i in data]
 
-    To create an input file for a given test, simply run the test with the `--bless` option
-    """
+    if get_origin(ty) is dict and ty.__args__[0] is str:
+        assert isinstance(data, dict)
 
-    def __init__(self):
-        path = os.environ["LLDB_BATCHMODE_INPUT_DATA_PATH"]
-        if os.path.isfile(path):
-            with open(path, "r") as f:
-                self.data = json.load(f)
-        else:
-            self.data = {
-                Target.NonWindowsGnu.value: [],
-                Target.WindowsGnu.value: [],
-                Target.WindowsMsvc.value: [],
-            }
+        if ty.__args__[1] is Variable:
+            return {k: from_dict(Variable, data[k]) for k in data.keys()}
+        if ty.__args__[1] is Child:
+            return {k: from_dict(Child, data[k]) for k in data.keys()}
 
-    def bless_variable(self, var_name: str, breakpoint: int):
-        """Updates the mapping with data generated from the given variable. Only affects the mapping
-        of the current target and breakpoint. This function **does not** write to the input file.
-        Update all necessary vars, then write the entire input data at once using
-        `RawInputData.save_blessing`"""
-        print(self)
-        valobj = FRAME.FindVariable(var_name)
-        if not valobj.IsValid():
-            # TODO error handling
-            raise Exception(f"<bless error: Cannot find variable {var_name}>")
+    # map dict -> dataclass, recursing for each field
+    if is_dataclass(ty):
+        assert isinstance(data, dict)
 
-        if len(self[TARGET]) <= breakpoint:
-            self[TARGET].append({})
-        self[TARGET][breakpoint][var_name] = asdict(Variable.from_lldb(valobj))
+        field_types = {
+            # First we try stripping any generics that may exist and look up the type in the
+            # builtins. This catchs int, float, bool, None, str, list, and dict. If none of those
+            # were found, we look up the type within this module.
+            f.name: f.type
+            for f in fields(ty)
+        }
 
-        print(self)
+        # if you've never seen this before, `**` is the splat operator. It expands a mapping type
+        # (in this case a dict) to keyword arguments. The ordering of the mapping does not matter,
+        # only that the mapping's keys match the functions keyword args, and `len(mapping)` == the
+        # number of keyword args.
+        try:
+            return ty(**{f: from_dict(field_types[f], data[f]) for f in data})
+        except KeyError as e:
+            print(f"Unable to convert dict to {ty}: Invalid field name {e}. If the test schema was \
+changed intentionally, use the `--bless` option to update test data to the new schema.")
 
-    def save_blessing(self):
-        """Writes the entirety of `self` to the input file. Used to finalize changes made by
-        one or more `RawInputData.bless_variable` calls."""
-        print("saving")
-        path = os.environ["LLDB_BATCHMODE_INPUT_DATA_PATH"]
-        with open(path, "w") as f:
-            json.dump(self.data, f)
-
-
-INPUT_DATA = RawInputData()
-
-
-def __lldb_init_module(debugger: SBDebugger, _dict):
-    pass
+    # for any other type, we don't need to do any processing
+    return data
 
 
 def decode_primitive(valobj: SBValue) -> int | float | bool | str:
@@ -137,6 +143,17 @@ def decode_primitive(valobj: SBValue) -> int | float | bool | str:
 
 
 @dataclass(slots=True)
+class Field:
+    name: str
+    type: str
+    offset: int
+
+    @staticmethod
+    def from_lldb(field: lldb.SBTypeMember) -> "Field":
+        return Field(field.GetName(), field.GetType().GetName(), field.GetOffsetInBytes())
+
+
+@dataclass(slots=True)
 class Type:
     name: str
     pretty_name: str
@@ -152,49 +169,16 @@ class Type:
     # static_fields: list[StaticField]
 
     @staticmethod
-    def from_lldb(type: SBType) -> Type:
+    def from_lldb(type: SBType) -> "Type":
         return Type(
             type.GetName(),
             type.GetDisplayTypeName(),
             type.GetByteSize(),
             type.GetByteAlign(),
-            [
-                Field.from_lldb(type.GetFieldAtIndex(i))
-                for i in range(type.GetNumberOfFields())
-            ],
+            [Field.from_lldb(type.GetFieldAtIndex(i)) for i in range(type.GetNumberOfFields())],
         )
         # TODO template args
         # self.generic_params = [lldb_lookup.get_template_args()]
-
-    @staticmethod
-    def from_json(type: dict) -> Type:
-        return Type(
-            type["name"],
-            type["pretty_name"],
-            type["size"],
-            type["align"],
-            [
-                Field(field["name"], field["type"], field["offset"])
-                for field in type["fields"]
-            ],
-        )
-
-
-@dataclass(slots=True)
-class Field:
-    name: str
-    type: str
-    offset: int
-
-    @staticmethod
-    def from_lldb(field: lldb.SBTypeMember) -> Field:
-        return Field(
-            field.GetName(), field.GetType().GetName(), field.GetOffsetInBytes()
-        )
-
-    @staticmethod
-    def from_json(field: dict) -> Field:
-        return Field(field["name"], field["type"], field["offset"])
 
 
 @dataclass(slots=True)
@@ -202,10 +186,10 @@ class Child:
     """Similar to `Variable`, but carries less information since we primarily test top-level values"""
 
     type: str
-    value: Primitive | dict[str, Child]
+    value: Primitive | dict[str, "Child"]
 
     @staticmethod
-    def from_lldb(child: SBValue) -> Child:
+    def from_lldb(child: SBValue) -> "Child":
         type: SBType = child.GetType()
 
         if type.GetBasicType() == lldb.eBasicTypeInvalid:
@@ -218,18 +202,6 @@ class Child:
 
         return Child(child.GetType().GetName(), value)
 
-    @staticmethod
-    def from_json(data: dict) -> Child:
-        type = data["type"]
-
-        value = data["value"]
-        if type(value) is dict:
-            value = {k: Child.from_json(v) for k, v in value}
-        else:
-            value = value
-
-        return Child(type, value)
-
 
 @dataclass(slots=True)
 class Variable:
@@ -237,7 +209,6 @@ class Variable:
     ~1:1 mapping from the json to a python class for convenience and consistency.
     """
 
-    name: str
     type: Type
     pretty_print: Optional[str]
     """`None` for aggregates with no summary provider"""
@@ -249,10 +220,7 @@ class Variable:
     children: dict[str, Child]
 
     @staticmethod
-    def from_lldb(var: SBValue) -> Variable:
-
-        name = var.GetName()
-
+    def from_lldb(var: SBValue) -> "Variable":
         sbtype = var.GetType()
         type = Type.from_lldb(sbtype)
 
@@ -284,7 +252,6 @@ class Variable:
         }
 
         return Variable(
-            name,
             type,
             pretty_print,
             value,
@@ -294,27 +261,123 @@ class Variable:
             children,
         )
 
-    @staticmethod
-    def from_json(name: str, data: dict) -> Variable:
-        type = Type.from_json(data["type"])
-        pretty_print = data.get("pretty_print", None)
-        synthetic = data.get("synthetic", None)
-        summary = data.get("summary", None)
-        format = data.get("format", None)
-        value = data.get("value", None)
 
-        children = {k: Child.from_json(v) for k, v in data["children"]}
+@dataclass(slots=True)
+class BlessMetadata:
+    """
+    Contains additional context about the tools at the time the test data was generated
+    """
 
-        return Variable(
-            name,
-            type,
-            pretty_print,
-            value,
-            synthetic,
-            summary,
-            format,
-            children,
+    python_version: str = ""
+    lldb_version: str = ""
+    # TODO
+    # lldb_feature_flags: str
+
+
+@dataclass(slots=True)
+class TargetData:
+    """
+    Per-target test data.
+    """
+
+    bless_metadata: BlessMetadata = field(default_factory=BlessMetadata)
+    breakpoints: list[dict[str, Variable]] = field(default_factory=list)
+    """Each element corresponds to one stopping point in the test. The element itself is a
+    dictionary mapping variable names to their respective test data."""
+
+
+@dataclass(slots=True, init=False)
+class TestData:
+    """
+    Top-level container for all test data.
+
+    Due to the differences between PDB and DWARF debug info, we cannot guarantee their output
+    will be identical. Since LLDB can handle both, we need to conditionally select the correct
+    test data to use.
+
+    Additionally, since there are differences in the internals of some structs based on OS (e.g.
+    `PathBuf`/`OsString`), we need to be aware of whether we're on Windows or not.
+
+    A global var `TARGET` is set to the current variant upon `lldb_test.py`'s instantiation using an
+    env var passed from `compiletest` and is not expected to change afterwards.
+    """
+
+    non_windows_gnu: TargetData
+    windows_gnu: TargetData
+    windows_msvc: TargetData
+
+    def __init__(self):
+        path = os.environ["LLDB_BATCHMODE_INPUT_DATA_PATH"]
+        if not os.path.isfile(path):
+            self.non_windows_gnu = TargetData()
+            self.windows_gnu = TargetData()
+            self.windows_msvc = TargetData()
+
+            return
+
+        with open(path, "r") as f:
+            data = json.load(f)
+
+        if BLESS and TARGET == Target.WindowsGnu:
+            self.windows_gnu = TargetData()
+        else:
+            self.windows_gnu = from_dict(TargetData, data["windows_gnu"])
+
+        if BLESS and TARGET == Target.WindowsMsvc:
+            self.windows_msvc = TargetData()
+        else:
+            self.windows_msvc = from_dict(TargetData, data["windows_msvc"])
+
+        if BLESS and TARGET == Target.NonWindowsGnu:
+            self.non_windows_gnu = TargetData()
+        else:
+            self.non_windows_gnu = from_dict(TargetData, data["non_windows_gnu"])
+
+    def get_target_data(self) -> TargetData:
+        """Retrieves data from the target specified by `compiletest`"""
+
+        if TARGET == Target.WindowsGnu:
+
+            return self.windows_gnu
+
+        if TARGET == Target.WindowsMsvc:
+            return self.windows_msvc
+
+        return self.non_windows_gnu
+
+    def bless_variable(self, var_name: str, breakpoint_idx: int):
+        """Updates the mapping with data generated from the given variable. Only affects the mapping
+        of the current target and breakpoint. This function **does not** write to the input file.
+        Update all necessary vars, then write the entire input data at once using
+        `RawInputData.save_blessing`"""
+        valobj = FRAME.FindVariable(var_name)
+        if not valobj.IsValid():
+            # TODO error handling
+            raise Exception(f"<bless error: Cannot find variable {var_name}>")
+
+        target_data = self.get_target_data()
+
+        if len(target_data.breakpoints) <= breakpoint_idx:
+            target_data.breakpoints.append({})
+
+        target_data.breakpoints[breakpoint_idx][var_name] = Variable.from_lldb(valobj)
+
+        print(self)
+
+    def save_blessing(self):
+        """Writes the entirety of `self` to the input file. Used to finalize changes made by
+        one or more `RawInputData.bless_variable` calls."""
+        print("saving")
+
+        self.get_target_data().bless_metadata = BlessMetadata(
+            sys.version, lldb.debugger.GetVersionString()
         )
+        path = os.environ["LLDB_BATCHMODE_INPUT_DATA_PATH"]
+        with open(path, "w") as f:
+            json.dump(asdict(self), f)
+
+
+INPUT_DATA = TestData()
 
 
 def get_summary_or_value(valobj: SBValue) -> str | None:
@@ -325,19 +388,17 @@ def get_summary_or_value(valobj: SBValue) -> str | None:
     return summary
 
 
-def run(var: str, breakpoint: int, frame: lldb.SBFrame):
+def run(var_name: str, breakpoint: int, frame: lldb.SBFrame):
     global FRAME
     FRAME = frame
-    # try:
-    bless = os.environ["LLDB_BATCHMODE_BLESS_TEST_DATA"] == "1"
-    if bless:
+
+    if BLESS:
         print("blessing")
-        INPUT_DATA.bless_variable(var, breakpoint)
+        INPUT_DATA.bless_variable(var_name, breakpoint)
 
-    print(INPUT_DATA)
-    check(Variable.from_json(var, INPUT_DATA[TARGET][breakpoint][var]))
+    check(var_name, INPUT_DATA.get_target_data().breakpoints[breakpoint][var_name])
 
-    print(f"{var}: Ok")
+    print(f"{var_name}: Ok")
 
 
 TYPE_UNPACK_FMT = {
@@ -402,8 +463,8 @@ def get_var(var_name: str) -> SBValue:
     return var
 
 
-def check(expected: Variable):
-    valobj: SBValue = get_var(expected.name)
+def check(var_name: str, expected: Variable):
+    valobj: SBValue = get_var(var_name)
 
     check_layout(valobj, expected)
     check_format(valobj, expected)
@@ -424,9 +485,7 @@ def check_layout(valobj: SBValue, expected: Variable):
     assert type.GetByteAlign() == exp_type.align
     assert len(type.fields) == len(exp_type.fields)
 
-    fields = {
-        Field(f.name, f.GetType().GetName(), f.GetOffsetInBytes()) for f in type.fields
-    }
+    fields = {Field(f.name, f.GetType().GetName(), f.GetOffsetInBytes()) for f in type.fields}
 
     assert fields == set(exp_type.fields)
 
@@ -463,9 +522,7 @@ def check_primitive(valobj: SBValue, expected: Variable | Child):
 
 
 def check_format(valobj: SBValue, expected: Variable):
-    fmt = (
-        valobj.GetTypeFormat().GetFormat() if valobj.GetTypeFormat().IsValid() else None
-    )
+    fmt = valobj.GetTypeFormat().GetFormat() if valobj.GetTypeFormat().IsValid() else None
 
     assert fmt == expected.format, (
         f"Invalid format for type {valobj.GetTypeName()}. Got: {valobj.GetTypeFormat()}"
@@ -473,11 +530,7 @@ def check_format(valobj: SBValue, expected: Variable):
 
 
 def check_aggregate(valobj: SBValue, expected: Variable):
-    synth = (
-        valobj.GetTypeSynthetic().GetData()
-        if valobj.GetTypeSynthetic().IsValid()
-        else None
-    )
+    synth = valobj.GetTypeSynthetic().GetData() if valobj.GetTypeSynthetic().IsValid() else None
     expected_synth = expected.synthetic
 
     assert synth == expected_synth, (
@@ -506,14 +559,14 @@ def check_synthetic(valobj: SBValue, synth_provider: str, expected: Variable):
     check_children(valobj, synth, expected.children)
 
 
-def check_children(valobj: SBValue, synth, expected: dict[str, Child]):
+def check_children(valobj: SBValue, synth: str, expected: dict[str, Child]):
     child_count = valobj.GetNumChildren()
     expected_child_count = len(expected)
     assert child_count == expected_child_count, (
         f"Expected {expected_child_count} children, got {child_count}"
     )
 
-    errors = []
+    errors: list[str] = []
 
     for k, v in expected.items():
         child = valobj.GetChildMemberWithName(k)
@@ -536,9 +589,7 @@ def check_children(valobj: SBValue, synth, expected: dict[str, Child]):
         raise Exception("\n".join(errors))
 
 
-def check_summary(
-    valobj: SBValue, provider_name: Optional[str], expected: Optional[str]
-):
+def check_summary(valobj: SBValue, provider_name: Optional[str], expected: Optional[str]):
     sb_summary = valobj.GetTypeSummary()
     assert (
         provider_name is None and not sb_summary.IsValid()
